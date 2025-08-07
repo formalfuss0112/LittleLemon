0be9e8d97e605972778c627b16f72597d27b3eae
formalfuss0112 <manojkchandrashekar1@gmail.com>
2025-08-07 14:38:03 +0530

    third commit

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+ 
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    number_of_guests = models.PositiveIntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.name} - {self.date} at {self.time}"
